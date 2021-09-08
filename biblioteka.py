52442332db5e479d21a28a77c4c43e015ff80283
dakinkkost <danik.kostins@gmail.com>
2021-09-08 11:09:31 +0300

    123

--- biblioteka.py
+++ biblioteka.py
@@ -1,1 +1,14 @@
-?|?260302
+class Shoes
+
+
+
+    def __init__(self, name, tips, materials, cena):
+        self.name = name
+        self.tips = tips
+        self.materials = materials
+        self.cena = cena
+
+    def myFirstFunction
+        return "Hi! "
+    def myNewFunction
+        return "Too expensive"
--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import biblioteka as shoes
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+firstShoes = shoes.Shoes('Gucci','Summer shoes', "ada ", 700 )
+secondShoes = shoes.Shoes("Adidas", "ada", 100)
+secondShoes.tips = "Boots"
+print(secondShoes.tips)
+print(myFirstFunction)
